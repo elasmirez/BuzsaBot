86fde67bdab522c0868e32c2dd6ce7fbb1f0936f
Alejandro Sáez <elasmirez@gmail.com>
2016-11-17 21:21:18 +0100

    Changing bot token

--- src/utils/Tokens.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class Tokens {
-
-	public static final String BOT_TOKEN = "249295953:AAGX0xmvOPR54TCtqq5w9ALihrZLtUDUXYQ";
-	public static final String BOT_NAME = "@BuzsaBot";
-
-
-}
